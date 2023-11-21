e411dc1c2d35e06ce3a7aba064774f9a3962b362
Gsaganichaitanya <chaitanyagasagani@gmail.com>
2023-11-21 15:47:31 +0530

    methodoverride inheritance

---
+++ HerbalProduct.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment17Part2
+{
+    class HerbalProduct : Product
+    {
+        private string herbsUsed;
+        private DateTime mfDate;
+        private DateTime expDate;
+
+        // Override methods to add details of Herbal Product fields
+        public override void ShowDetails()
+        {
+            base.ShowDetails(); // Call base class method first
+            Console.WriteLine($"Herbs Used: {herbsUsed}");
+            Console.WriteLine($"Manufacturing Date: {mfDate.ToShortDateString()}");
+            Console.WriteLine($"Expiry Date: {expDate.ToShortDateString()}");
+        }
+
+        public override void SetInformation(int id, string name, double price)
+        {
+            base.SetInformation(id, name, price); // Call base class method first
+            Console.Write("Enter Herbs Used: ");
+            herbsUsed = Console.ReadLine();
+            Console.Write("Enter Manufacturing Date (yyyy-MM-dd): ");
+            mfDate = DateTime.Parse(Console.ReadLine());
+            Console.Write("Enter Expiry Date (yyyy-MM-dd): ");
+            expDate = DateTime.Parse(Console.ReadLine());
+        }
+    }
+}
---
+++ Product.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment17Part2
+{
+    public class Product
+    {
+        
+            protected int pid;
+            protected string pname;
+            protected double pprice;
+
+            public virtual void ShowDetails()
+            {
+                Console.WriteLine($"Product ID: {pid}");
+                Console.WriteLine($"Product Name: {pname}");
+                Console.WriteLine($"Product Price: {pprice:C}");
+            }
+
+            
+            public virtual void SetInformation(int id, string name, double price)
+            {
+                pid = id;
+                pname = name;
+                pprice = price;
+            }
+        }
+    }
+
